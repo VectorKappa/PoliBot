3cbf55ecebc01bfd19bc63b5ac10ca5bcc6c7f62
VectorKappa <piotrpatalong@gmail.com>
2020-03-12 23:21:51 +0100

    Wersja 0.0.7 [ x ] Przywrócono funkcjonalność logowania [ x ] Spolonizowano stringi [ x ] Podzielono kod na moduły [ x ] Usunięto niepotrzebne komendy [ ] Dodano przyznawanie rangi [ ] Dodano przerwy

--- musicbot.py
+++ musicbot.py
@@ -1,9 +1,10 @@
 import asyncio
-
+import json
 import discord
 import youtube_dl
 
 import credentials
+
 from discord.ext import commands
 
 # Suppress noise about console usage from errors
@@ -11,9 +12,11 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 
 token = credentials.getToken()
 
-wiadomosc_info = "```To jest bot przeznaczony dla Zespołu Szkół Poligraficzno-Mechanicznych im. Armii Krajowej w Katowicach. \n Aby uzyskać listę komend, wpisz $komendy \n Więcej info: https://github.com/VectorKappa/PoliBot```"
+lista_klas = ("1a Tpd/pi", "1a Tgpc", "1b Tgpc", "1cTi", "1d Ti", "1d Tm", "1eg Tgpc", "1fg Ti", "1fg Tm", "1gg Ti", "2a Tpd", "2a Tgpc",
+              "2c Ti", "2d Ti", "2d Tm", "3a Tpd", "3a Tgpc", "3b Tgpc", "3c Ti", "3d Ti", "3d Tm", "4a Tpd", "4a Tm", "4b Tcpg", "4c Ti", "4d Ti")
+wiadomosc_info = "To jest bot przeznaczony dla Zespołu Szkół Poligraficzno-Mechanicznych im. Armii Krajowej w Katowicach. \n Aby uzyskać listę komend, wpisz $komendy \n Więcej info: https://github.com/VectorKappa/PoliBot"
 lista_komend = "```Lista komend bota: \n $komendy - wysyła tę listę komend \n $radio - kontroluje radio, aby uzyskać pomoc dotyczącą subkomend użyj $komendy radio \n ```"
-
+wybor_klasy = "```Witamy na szkolnym serwerze ZSPM! Podaj nam klasę, do której uczęszczasz:```"
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
@@ -38,6 +41,7 @@ ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
 class YTDLSource(discord.PCMVolumeTransformer):
+    """Ukradzione z wbudowanych przykładów discord.py"""
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
 
@@ -59,13 +63,27 @@ class YTDLSource(discord.PCMVolumeTransformer):
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
-class Music(commands.Cog):
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def info(self, ctx):
+        await ctx.send(wiadomosc_info)
+
+    @commands.command()
+    async def komendy(self, ctx):
+        await ctx.send('Wysłałem ci listę komend na PM')
+        await ctx.author.send(lista_komend)
+
+
+class Muzyka(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def join(self, ctx, *, channel: discord.VoiceChannel):
-        """Joins a voice channel"""
+        """Dołącza do kanału głosowego"""
 
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
@@ -73,110 +91,101 @@ class Music(commands.Cog):
         await channel.connect()
 
     @commands.command()
-    async def play(self, ctx, *, query):
-        """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(source, after=lambda e: print(
-            'Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(query))
-
-    @commands.command()
-    async def yt(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
+    async def play(self, ctx, *, url):
+        """Odtwarza utwór z URLa"""
 
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop)
             ctx.voice_client.play(player, after=lambda e: print(
-                'Player error: %s' % e) if e else None)
+                'Błąd odtwarzacza: %s' % e) if e else None)
 
-        await ctx.send('Now playing: {}'.format(player.title))
+        await ctx.send('Teraz leci: {}'.format(player.title))
 
     @commands.command()
     async def stream(self, ctx, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
+        """Odtwarza na żywo z URLa"""
 
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
             ctx.voice_client.play(player, after=lambda e: print(
-                'Player error: %s' % e) if e else None)
+                'Błąd odtwarzacza: %s' % e) if e else None)
 
-        await ctx.send('Now playing: {}'.format(player.title))
+        await ctx.send('Teraz leci: {}'.format(player.title))
 
     @commands.command()
     async def volume(self, ctx, volume: int):
-        """Changes the player's volume"""
+        """Zmienia głośność odtwarzania"""
 
         if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
+            return await ctx.send("Nie podłączono do żadnego kanału.")
 
         ctx.voice_client.source.volume = volume / 100
-        await ctx.send("Changed volume to {}%".format(volume))
-    
-    @commands.command()
-    async def happyturzyn(self, ctx):
-        return await ctx.send("Happy turzyn:")
-        return await ctx.send(file=discord.File('.\\images\\happyturzyn.png'))
-
-    @commands.command()
-    async def happyzuber(self, ctx):
-        return await ctx.send("Happy Zuber:")
-        return await ctx.send(file=discord.File('.\\images\\zuber_approves.jpeg'))
-
-    @commands.command()
-    async def info(self, ctx):
-        await ctx.send(wiadomosc_info)
-    
-    @commands.command()
-    async def komendy(self, ctx):
-        await ctx.send('Wysłałem ci listę komend na PM')
-        await ctx.author.send(lista_komend)
+        await ctx.send("Zmieniono głośność na {}%".format(volume))
 
     @commands.command()
     async def stop(self, ctx):
-        """Stops and disconnects the bot from voice"""
+        """Zatrzymuje bota i rozłącza go"""
 
         await ctx.voice_client.disconnect()
 
     @play.before_invoke
-    @yt.before_invoke
     @stream.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
             if ctx.author.voice:
                 await ctx.author.voice.channel.connect()
             else:
-                await ctx.send("You are not connected to a voice channel.")
+                await ctx.send("Nie jesteś podłączony do żadnego kanału głosowego.")
                 raise commands.CommandError(
-                    "Author not connected to a voice channel.")
+                    "Autor wiadomości nie jest podpięty do serwera.")
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
 
+class Logger(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.author == bot.user:
+            return
+        else:
+            print(f"{ctx.author} napisał {ctx.content} na {ctx.channel}")
+
+
+class Przydzielaczka(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, ctx):
+        await ctx.send(wybor_klasy)
+        print(f"{ctx} dołączył(-a) na serwer.")
+
+    @commands.command()
+    async def klasa(self, ctx, wybrana_klasa):
+        if wybrana_klasa in lista_klas:
+            pass
+        else:
+            raise commands.ArgumentParsingError(
+                str(ctx.author) + " podał złą klasę " + str(ctx.message.content))
+        pass
+
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"),
-                   description='Relatively simple music bot example')
+                   description=wiadomosc_info, help_command=None)
 
 
 @bot.event
 async def on_ready():
-    print('Logged in as {0} ({0.id})'.format(bot.user))
-    print('---------------------------------')
+    print('Zalogowano jako {0} ({0.id})'.format(bot.user))
+    print('-------------------------------------------------')
 
-#doesn't fucking work
+# Przełączanie funkcjonalności (Wykomentować niechciane linijki)
+bot.add_cog(Info(bot))
+bot.add_cog(Muzyka(bot))
+bot.add_cog(Przydzielaczka(bot))
+bot.add_cog(Logger(bot))
 
-#@bot.event
-#async def on_message(message):
-#    print('Message from {0.author}: \"{0.content}\" on {0.channel}'.format(message))
-#    return
-
-
-#if message.content.startswith('$radio dodaj'):
-#if message.content.startswith('$radio') and message.channel == "muzyczna-złota-rybka" or message.channel == "zabawy-z-automatem" or message.channel == "przełożeni-polibota":
-#like = '\N{THUMBS UP SIGN}'
-#dislike = '\N{THUMBS DOWN SIGN}'
-#await message.add_reaction(like)
-#await message.add_reaction(dislike)
-
-bot.add_cog(Music(bot))
 bot.run(token)
